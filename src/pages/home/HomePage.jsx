500b3a8e4f196e6a69f2c6d8e73cfe66e14aab20
zyad667-ui <ziadfiach4@gmail.com>
2025-05-17 14:52:04 +0100

    second commit

---
+++ src/pages/home/HomePage.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useNavigate } from "react-router-dom";
+import navbar from "../../components/navbar";
+import books from "../../components/books";
+
+
+
+
+const HomePage = () => {
+
+    return (
+        <>
+       < navbar />
+
+
+
+        </>
+    );
+};
+
+export default HomePage;
